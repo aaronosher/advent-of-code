518c54a0fc5de11fb7e3fa476a4c10997a93e636
Aaron Osher <aaron@aaronosher.io>
2019-12-06 00:32:58 +0000

    Add 2 part 1

---
+++ 2019/02/01.hs
@@ -0,0 +1,37 @@
+import System.Environment
+import qualified Data.Text as T
+
+readF :: [T.Text] -> [Int]
+readF = map (read . T.unpack)
+
+replaceN :: Int -> [a] -> a -> [a]
+replaceN loc xs val = x ++ val : ys
+    where (x,_:ys)  = splitAt loc xs
+
+addOp :: [Int] -> Int -> [Int]
+addOp xs pos    = replaceN res xs sum
+    where opp1  = xs!!(xs!!(pos+1))
+          opp2  = xs!!(xs!!(pos+2))
+          res   = xs!!(pos+3)
+          sum   = opp1 + opp2
+
+mulOp :: [Int] -> Int -> [Int]
+mulOp xs pos    = replaceN res xs diff
+    where opp1  = xs!!(xs!!(pos+1))
+          opp2  = xs!!(xs!!(pos+2))
+          res   = xs!!(pos+3)
+          diff  = opp1 * opp2
+
+opcodes :: [Int] -> Int -> [Int]
+opcodes xs pos
+        | (xs!!pos == 1) = opcodes (addOp xs pos) (pos + 4)
+        | (xs!!pos == 2) = opcodes (mulOp xs pos) (pos + 4)
+        | (xs!!pos == 99) = xs
+        | otherwise = error "Invalid opcode"
+    
+main = do
+    [f]     <- getArgs
+    rawInput <- readFile f
+    let input = (lines rawInput)!!0 
+    let program = readF (T.splitOn (T.pack ",") (T.pack input))
+    print ((opcodes program 0)!!0)
